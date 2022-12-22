ede482ed9946472f31408a268adcda69b80da980
Username-Ewgeniy <114725663+Username-Ewgeniy@users.noreply.github.com>
2022-12-23 02:00:54 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,12 +4,13 @@ from config import TOKEN, exchanges
 import traceback
 
 
+
 bot = telebot.TeleBot(TOKEN)
 
 
 @bot.message_handler(commands=['start', 'help'])
 def start(message: telebot.types.Message):
-    text = "Привет!"
+    text = "Приветствие!"
     bot.send_message(message.chat.id, text)
 
 
@@ -20,15 +21,45 @@ def values(message: telebot.types.Message):
         text = '\n'.join((text, i))
     bot.reply_to(message, text)
 
+@bot.message_handler(commands=['convert'])
+def convert(message: telebot.types.Message):
+    text = 'Выберите валюту, из которой конвертировать'
+    bot.send_message(message.chat.id, text)
+    bot.register_next_step_handler(message, base_handler)
+
+def base_handler(message: telebot.types.Message):
+    base = message.text.strip()
+    text = 'Выберите валюту, в которую конвертировать'
+    bot.send_message(message.chat.id, text)
+    bot.register_next_step_handler(message, sym_handler, base)
+
+def sym_handler(message: telebot.types.Message, base):
+    sym = message.text.strip()
+    text = 'Выберите количество конвертируемой валюты'
+    bot.send_message(message.chat.id, text)
+    bot.register_next_step_handler(message, amount_handler, base, sym)
+
+def amount_handler(message: telebot.types.Message, base, sym):
+    amount = message.text.strip()
+    try:
+        new_price = Convertor.get_price(base, sym, amount)
+    except APIException as e:
+        bot.send_message(message.chat.id, f"Ошибка конвертации: \n {e}")
+    else:
+        text = new_price
+        bot.send_message(message.chat.id, text)
+
+
+
 
 @bot.message_handler(content_types=['text'])
 def converter(message: telebot.types.Message):
-    values = message.text.split(' ')
+    val = message.text.split()
     try:
-        if len(values) != 3:
+        if len(val) != 3:
             raise APIException('Неверное количество параметров!')
 
-        answer = Convertor.get_price(*values)
+        answer = Convertor.get_price(*val)
     except APIException as e:
         bot.reply_to(message, f"Ошибка в команде:\n{e}")
     except Exception as e:
@@ -36,6 +67,5 @@ def converter(message: telebot.types.Message):
         bot.reply_to(message, f"Неизвестная ошибка:\n{e}")
     else:
         bot.reply_to(message, answer)
-
 
 bot.polling()
